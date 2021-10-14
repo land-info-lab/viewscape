a2599f8b1d2d35c8e6dd3a550d483d5b71b81587
nfox29 <nf2g13@soton.ac.uk>
2021-10-14 15:17:30 -0400

    updated first test

--- tests/testthat/test-calculate_viewshed.R
+++ tests/testthat/test-calculate_viewshed.R
@@ -1,2 +1,21 @@
 context("Calculate viewshed")
 
+test_that("runs correctly", {
+
+  #load files
+  test_dsm <- raster::raster(system.file("test_data\\test_dsm.tif",
+                                         package ="viewscape"))
+
+  test_viewpoint <- sf::read_sf(system.file("test_data\\test_viewpoint.shp",
+                                               package = "viewscape"))
+
+  test_viewpoint <- sf::st_coordinates(test_viewpoint)
+
+  test_viewpoint <- c(test_viewpoint[,1], test_viewpoint[,2])
+
+  #run function
+  test_function <- viewscape::calculate_viewshed(dsm = test_dsm,
+                                                 viewpoint = test_viewpoint)
+
+
+})
