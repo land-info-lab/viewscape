21dfd2dc6b9cc67fe5cd5534ada61112a6c23840
billbillbilly <xiaohaoy@umich.edu>
2021-11-23 11:00:46 -0500

    - add default heights for viewpoint and other target points

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 #' @noRd
 
-samplealongline <- function(dsm, modified_dsm=NULL, dem=NULL, viewpoint, h, samplecoordinates){
+samplealongline <- function(dsm, modified_dsm=NULL, dem=NULL, viewpoint, h1, h2, samplecoordinates){
   #this fuction can sample elevations along the straight line from viewpoint to a certain coordinates on raster surface
   #and also sample elevations on this line corresponding to the locations of sampled elevations on raster
 
@@ -8,31 +8,34 @@ samplealongline <- function(dsm, modified_dsm=NULL, dem=NULL, viewpoint, h, samp
   # modified_dsm is another dsm, where the elevation above crown base of trees was removed
   # dem is the raster of terrain without any vertical object. If input of dsm is already digital elevation model, this arguement can be ignored
   # viewpoint is a matrix including x,y coordinates
-  # h is the height of viewpoint. the unit of input should be meter
+  # h1 is the height of viewpoint. the unit of input should be meter
+  # h2 is the height of sample coordinates. the unit of input should be meter
   # samplecoordinates is the coordinates of a certain cell(point) of a DSM
   if(grepl("units=m",test_dsm@crs@projargs, fix = TRUE)){ # if unit is meter
-    h <- h
+    h1 <- h1
+    h2 <- h2
   }else if(grepl("units=ft",test_dsm@crs@projargs, fix = TRUE)){ # if unit is feet
-    h <- h * 3.281
+    h1 <- h1 * 3.281
+    h2 <- h2 * 3.281
   }
   viewline = sqrt( (viewpoint[1]-samplecoordinates[1])^2 + (viewpoint[2]-samplecoordinates[2])^2 ) #distance from viewpoint to sample point
   #sample a raster along a straight line between two points
   steps = 1 + round(viewline/ min(raster::res(dsm))) #try to match the sampling size to the raster resolution
   xc = viewpoint[1] + (0:steps) * (samplecoordinates[1]-viewpoint[1])/steps #x coordinates of samples along the viewline
   yc = viewpoint[2] + (0:steps) * (samplecoordinates[2]-viewpoint[2])/steps #y coordinates of samples along the viewline
-  z_viewpoint = dsm[raster::cellFromXY(dsm,cbind(viewpoint[1],viewpoint[2]))] + h #the height of viewpoints based on dsm
+  z_viewpoint = dsm[raster::cellFromXY(dsm,cbind(viewpoint[1],viewpoint[2]))] + h1 #the height of viewpoints based on dsm
 
   if(is.null(modified_dsm) == FALSE){# if input modified_dsm is detected, compare the height of viewpoint on dsm to the one on dem
     if(is.null(dem) == TRUE){# request dsm
       stop("dem is NULL")
     }
-    z_viewpoint2 = dem[raster::cellFromXY(dem,cbind(viewpoint[1],viewpoint[2]))] + h #the height of viewpoints based on dem
+    z_viewpoint2 = dem[raster::cellFromXY(dem,cbind(viewpoint[1],viewpoint[2]))] + h1 #the height of viewpoints based on dem
     z_viewpoint_delta <- z_viewpoint - z_viewpoint2
     if(z_viewpoint_delta >= 3){# if the height of viewpoint on dsm is higher than the one on dem, modified_dsm will be used for viewshed analysis
       dsm <- modified_dsm
     }
   }
-  z_sample = dsm[raster::cellFromXY(dsm,cbind(samplecoordinates[1],samplecoordinates[2]))] #set elevation of sample
+  z_sample = dsm[raster::cellFromXY(dsm,cbind(samplecoordinates[1],samplecoordinates[2]))] + h2 #set elevation of sample
   zc = dsm[raster::cellFromXY(dsm,cbind(xc,yc))] #extract elevation from dsm to the samples along the line based on corresponding coordinates
   # get elevation along the straight line at the same position of each step point
   if(z_viewpoint<z_sample){#when elevation of viewpoint is lower than sample
@@ -45,16 +48,17 @@ samplealongline <- function(dsm, modified_dsm=NULL, dem=NULL, viewpoint, h, samp
   data.frame(x=xc, y=yc, z=zc, zl=zl)
 }
 
-visiblesample <- function(dsm, modified_dsm, dem, viewpoint, h, samplecoordinates){
+visiblesample <- function(dsm, modified_dsm, dem, viewpoint, h1, h2, samplecoordinates){
   #this function determines if the positions of sampled elevations can be seen from the viewpoint
 
   # dsm is raster layer of digital surface model/digital elevation model
   # modified_dsm is another dsm, where the elevation above crown base of trees was removed
   # dem is the raster of terrain without any vertical object. If input of dsm is already digital elevation model, this arguement can be ignored
   # viewpoint is a matrix including x,y coordinates
-  # h is the height of viewpoint
+  # h1 is the height of viewpoint. the unit of input should be meter
+  # h2 is the height of sample coordinates. the unit of input should be meter
   # samplecoordinates is the coordinates of a certain cell(point) of a DSM
-  xyzzl <- samplealongline(dsm, modified_dsm = modified_dsm, dem = dem, viewpoint, h, samplecoordinates)
+  xyzzl <- samplealongline(dsm, modified_dsm = modified_dsm, dem = dem, viewpoint, h1, h2, samplecoordinates)
   xyzzl$z_delta <- xyzzl$zl - xyzzl$z
   xyzzl <- base::subset(xyzzl, is.na(xyzzl$z_delta)==FALSE , select = x:z_delta)
   min_delta <- min(xyzzl[5])
@@ -66,14 +70,15 @@ visiblesample <- function(dsm, modified_dsm, dem, viewpoint, h, samplecoordinate
   }
 }
 
-calculate_viewshed <- function(dsm, under=NULL, dem=NULL, viewpoint, h, sample_points=NULL, r = NULL){
+calculate_viewshed <- function(dsm, under=NULL, dem=NULL, viewpoint, h1=1.7, h2=0, sample_points=NULL, r = NULL){
   # calculate viewshed based on a certain viewpoint
 
   # dsm is raster layer of digital surface model/digital elevation model
   # under is raster layer of digital surface model where the elevation above crown base of trees was removed
   # dem is the raster of terrain without any vertical object. If input of dsm is already digital elevation model, this arguement can be ignored
   # viewpoint is a matrix including x,y coordinates
-  # h is the height of viewpoint
+  # h1 is the height of viewpoint. the unit of input should be meter
+  # h2 is the height of sample coordinates. the unit of input should be meter
   # sample_points is a matrix of points that are converted from DSM. This can be ignored. (it is defaulted as NULL)
   # r is the radius for viewshed analysis. (it is defaulted as NULL)
   if(is.null(r) == TRUE){
@@ -101,7 +106,7 @@ calculate_viewshed <- function(dsm, under=NULL, dem=NULL, viewpoint, h, sample_p
   for(i in 1:nrow(sample_points)){
     #calculate the distance between viewpoint and each cell(point) of(on) the raster(DSM) and extract visible points.
     samplecoordinates <- sample_points[i,] #read coordinates of each position (cell in raster) one by one
-    visibles <- visiblesample(dsm, modified_dsm = under, dem = dem, viewpoint, h, samplecoordinates) #check visibility
+    visibles <- visiblesample(dsm, modified_dsm = under, dem = dem, viewpoint, h1, h2, samplecoordinates) #check visibility
     if(is.null(visibles) == FALSE){# if the position of sample can be seen
       visiblepoints <- rbind(visiblepoints, visibles) #add this position into the matrix
     }
