e62e0d403639445970b7b98053b0607359adb12a
billbillbilly <xiaohaoy@umich.edu>
2023-10-29 18:56:02 -0400

    - update

--- src/multiLabel.cpp
+++ src/multiLabel.cpp
@@ -21,7 +21,7 @@ Rcpp::List multiLabel(Rcpp::NumericMatrix &vpts,
   #ifdef _WIN32
     #pragma omp parallel shared(output, dsm_list, max_dis, vpth, h, vptnum) num_threads(workers)
       {
-        #pragma omp schedule(static) for
+        #pragma omp for
         {
           for (int i = 0; i < vptnum; i++) {
             const Rcpp::NumericMatrix sub_dsm = dsm_list[i];
