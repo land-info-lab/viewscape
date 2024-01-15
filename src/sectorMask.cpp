12284c9ece8d6851cb00c68c54d7905b60f30c6c
billbillbilly <xiaohaoy@umich.edu>
2024-01-15 02:53:52 -0500

    sectorMask

---
+++ src/sectorMask.cpp
@@ -0,0 +1,9 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+Rcpp::IntegerMatrix sectorMask(
+    const Rcpp::IntegerMatrix &viewshed,
+    const Rcpp::NumericVector fov) {
+  return 0;
+}
